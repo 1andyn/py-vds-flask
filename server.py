a6d6e6150cb886825c3d508cd8903bcfcb729dd4
1andyn <andy.work.dev@gmail.com>
2020-08-06 22:15:55 -1000

    Baseline Server file for Auth0 Authorization

---
+++ server.py
@@ -0,0 +1,143 @@
+import authfile
+import driver
+import json
+import jwt
+from six.moves.urllib.request import urlopen
+from functools import wraps
+from flask import Flask, request, jsonify, _request_ctx_stack
+
+AUTH0_DOMAIN = authfile.a_dm
+API_AUDIENCE = authfile.a_ap
+ALGORITHMS = ["RS256"]
+
+app = Flask(__name__)
+
+# establish connection to database
+connection = driver.Database()
+
+
+@app.route('/test', methods=['GET'])
+def test_api():
+    return jsonify({'result': "hello it is me your friend"})
+
+
+# Format error response and append status code
+def get_token_auth_header():
+    """Obtains the Access Token from the Authorization Header """
+
+    auth = request.headers.get("Authorization", None)
+    if not auth:
+        raise AuthError({"code": "authorization_header_missing",
+                         "description":
+                             "Authorization header is expected"}, 401)
+
+    # Splits out Auth Header to verify length and contents
+    parts = auth.split()
+    if parts[0].lower() != "bearer":
+        raise AuthError({"code": "invalid_header",
+                         "description":
+                             "Authorization header must start with"
+                             " Bearer"}, 401)
+    elif len(parts) == 1:
+        raise AuthError({"code": "invalid_header",
+                         "description": "Token not found"}, 401)
+    elif len(parts) > 2:
+        raise AuthError({"code": "invalid_header",
+                         "description":
+                             "Authorization header must be Bearer token"}, 401)
+
+    token = parts[1]
+    return token
+
+
+def requires_auth(f):
+    """Determines if the Access Token is valid"""
+
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = get_token_auth_header()
+        jsonurl = urlopen("https://" + AUTH0_DOMAIN + "/.well-known/jwks.json")
+        jwks = json.loads(jsonurl.read())
+        unverified_header = jwt.get_unverified_header(token)
+        rsa_key = {}
+        for key in jwks["keys"]:
+            if key["kid"] == unverified_header["kid"]:
+                rsa_key = {
+                    "kty": key["kty"],
+                    "kid": key["kid"],
+                    "use": key["use"],
+                    "n": key["n"],
+                    "e": key["e"]
+                }
+
+        if rsa_key:
+            try:
+                payload = jwt.decode(
+                    token,
+                    rsa_key,
+                    algorithms=ALGORITHMS,
+                    audience="https://" + API_AUDIENCE,
+                    issuer="https://" + AUTH0_DOMAIN + "/"
+                )
+
+            except jwt.ExpiredSignatureError:
+                raise AuthError({"code": "token_expired",
+                                 "description": "token is expired"}, 401)
+
+            except jwt.InvalidAudienceError:
+                raise AuthError({"code": "invalid audience",
+                                 "description": "audience is invalid"}, 401)
+
+            except jwt.InvalidAlgorithmError:
+                raise AuthError({"code": "invalid algorithm",
+                                 "description": "algorithm is invalid"}, 401)
+
+            except jwt.InvalidSignatureError:
+                raise AuthError({"code": "invalid signature",
+                                 "description": "signature is invalid"}, 401)
+
+            except jwt.InvalidTokenError:
+                raise AuthError({"code": "invalid token",
+                                 "description": "token is invalid"}, 401)
+
+            except jwt.InvalidIssuerError:
+                raise AuthError({"code": "invalid issuer",
+                                 "description": "issuer is invalid"}, 401)
+            """
+            except Exception:
+                raise AuthError({"code": "invalid_header",
+                                 "description": "Unable to parse authentication token."
+                                 "more:": Exception. }, 401)
+ """
+            _request_ctx_stack.top.current_user = payload
+            return f(*args, **kwargs)
+        raise AuthError({"code": "invalid_header",
+                         "description": "Unable to find appropriate key"}, 401)
+
+    return decorated
+
+
+# Error handler
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
+
+
+@app.errorhandler(AuthError)
+def handle_auth_error(ex):
+    response = jsonify(ex.error)
+    response.status_code = ex.status_code
+    return response
+
+
+# This needs authentication
+@app.route("/retrieve")
+@requires_auth
+def private():
+    response = "Hello from a private endpoint! You need to be authenticated to see this."
+    return jsonify(message=response)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
